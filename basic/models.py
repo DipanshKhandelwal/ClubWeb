fa8ae606be8c6acb387b9a958590c1c455d0a2db
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-09 13:02:55 +0530

    Updated models.py, Added model Post

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+CLUB_NAMES = (
+    ('ArtClub','ARTCLUB'),
+    ('DanceClub','DANCECLUB'),
+    ('MusicClub','MUSICCLUB'),
+    ('CodingClub','CODINGCLUB'),
+)
+
+class Post(models.Model):
+    club_name = models.CharField(max_length=10, choices=CLUB_NAMES)
+    title = models.CharField(max_length=120)
+    information = models.TextField()
+    post_time = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
