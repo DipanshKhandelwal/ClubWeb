ebe812f30f05004ede7f14333f781c7536d35d24
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-10-06 17:01:25 +0530

    Added event detail page

--- basic/urls.py
+++ basic/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
-    url(r'^(?P<slug>[-\w\d]+)/$', views.club_page, name='club_page'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^clubs/(?P<slug>[-\w\d]+)/$', views.club_page, name='club_page'),
+    url(r'^posts/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^events/(?P<pk2>\d+)/$', views.event_detail, name='event_detail'),
+
 ]
--- basic/views.py
+++ basic/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Club, Post
+from .models import Club, Post, Event
 
 
 # Create your views here.
 def home_page(request):
     clubs = Club.objects.all()
+    events = Event.objects.all()
     posts = Post.objects.all()
-    return render(request, 'basic/home_page.html', {'Clubs': clubs, 'Posts': posts})
+    return render(request, 'basic/home_page.html', {'Clubs': clubs, 'Posts': posts, 'Events': events})
 
 
 def post_detail(request, pk):
     clubs = Club.objects.all()
+    events = Event.objects.all()
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'basic/post_detail.html', {'Clubs': clubs, 'post': post})
+    return render(request, 'basic/post_detail.html', {'Clubs': clubs, 'post': post, 'Events': events})
+
+
+def event_detail(request, pk2):
+    clubs = Club.objects.all()
+    events = Event.objects.all()
+    event = get_object_or_404(Post, pk=pk2)
+    return render(request, 'basic/event_detail.html', {'Clubs': clubs, 'event': event, 'Events': events})
 
 
 def club_page(request, slug):
     x = Club.objects.all()
+    events = Event.objects.all()
     t = False
     slug = slug.lower()
     clubs = Club.objects.all()
@@ -25,7 +35,7 @@ def club_page(request, slug):
             t = True
             break
     if t:
-        return render(request, 'basic/club_page.html', {'club': i, 'Clubs': x})
+        return render(request, 'basic/club_page.html', {'club': i, 'Clubs': x, 'Events': events})
     else:
-        return render(request, 'basic/error404.html', {'Clubs': x})
+        return render(request, 'basic/error404.html', {'Clubs': x, 'Events': events})
 
