6142e83afea064000b65961accb69f53e31d6cfe
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-10-06 13:31:19 +0530

    added post detail

--- basic/urls.py
+++ basic/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
     url(r'^(?P<slug>[-\w\d]+)/$', views.club_page, name='club_page'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- basic/views.py
+++ basic/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Club, Post
 from django.http import HttpResponse
 
@@ -8,6 +8,11 @@ def home_page(request):
     clubs = Club.objects.all()
     posts = Post.objects.all()
     return render(request, 'basic/home_page.html', {'Clubs': clubs, 'Posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'basic/post_detail.html', {'post': post})
 
 
 def club_page(request, slug):
