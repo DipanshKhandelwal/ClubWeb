bc968c993e0fee21c9c9411614d0e2e82cffffe4
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-10-25 02:26:01 +0530

    Updated home page url in posts

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.home_page, name='posts_home_page'),
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
