2cfb8a823949f4638cc17a1fc4d117fd304b7369
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-10-25 02:25:49 +0530

    Updated home page url in clubs

--- clubs/urls.py
+++ clubs/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.home_page, name='clubs_home_page'),
     url(r'^(?P<slug>[-\w\d]+)/$', views.club_page, name='club_page'),
 ]
